7fec13b829c5c25be8ff48b32b4658898cf98db2
Robin Kamo <robin.kamo@live.se>
2020-11-18 11:51:22 +0100

    My first powershell commit

--- GitDemoNET20D/Program.cs
+++ GitDemoNET20D/Program.cs
@@ -40,16 +40,46 @@ namespace GitDemoNET20D
 
             //Övning 2
             //Ett sätt
-            int[] firstOne = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            // int[] firstOne = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             // int[] secondOne = new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
             //    Console.WriteLine(string.Join(',', firstOne));
             //  Console.WriteLine(string.Join(',', secondOne));
 
             //Ett annat sätt
-            Console.WriteLine(string.Join(',', firstOne));
-            Console.WriteLine(string.Join(',', firstOne.Reverse()));
+            // Console.WriteLine(string.Join(',', firstOne));
+            //Console.WriteLine(string.Join(',', firstOne.Reverse()));
 
+            string[] stringArray = new string[]
+            {"January",
+                "feb",
+                "mars",
+                "april",
+                "maj",
+                "Juni",
+                "Juli",
+                "agusti",
+                "september",
+                "oktober",
+                "November",
+                "december"
+            };
+
+            string number;
+            int choice;
+
+            do
+            {
+
+                Console.WriteLine("Select a number from 1-12");
+                number = Console.ReadLine();
+                choice = Convert.ToInt32(number);
+                if(choice <= 12)
+                {
+                    Console.WriteLine(stringArray[choice - 1]);
+                }
+
+            } while (choice < 12);
 
 
 
